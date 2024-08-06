5207c33d551ca2db4c7dd046bf25010f63e8b0b8
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-08-06 13:38:38 +0530

    Add files via upload

---
+++ Countstring.java
@@ -0,0 +1,11 @@
+ class Countstring {
+    public static void main(String[] args) {
+        int num = 53646;
+        int count=0;
+        while(num!=0){
+            num=num/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
