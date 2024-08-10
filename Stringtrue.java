337b01438d593aee22d3f89650ba30ca3bce2fe3
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-08-10 22:01:32 +0530

    Add files via upload

---
+++ Stringtrue.java
@@ -0,0 +1,15 @@
+public class Stringtrue {
+   public static void main(String[] args) {
+    String s="12365";
+    int l=s.length()-1;
+    for(int i=0;i>l;i++){
+        if(9<=i){
+            System.out.println("valid");
+        }else{
+            System.out.println("not valid");
+                    
+        }
+    }
+    
+   } 
+}
