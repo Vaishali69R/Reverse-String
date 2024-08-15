df52a3263ff15bfa342b9830876c4ceeb65787b1
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-08-15 23:54:27 +0530

    Add files via upload

---
+++ StringAssi1.java
@@ -0,0 +1,15 @@
+class StringAssi1 {
+    public static void main(String[] args) {
+        String s= "Chatgpt";
+        char ch = 'o';
+        for(int i =0; i<s.length();i++){
+        char ch1 = s.charAt(0);
+        if(s.charAt()ch){
+            System.out.println(s);
+
+        }
+        System.out.println(s.charAt(0));
+        break;
+        }
+    }
+}
