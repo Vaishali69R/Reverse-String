801c02588157b46c3a7b6c30fbea48f6cec86f81
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-08-23 23:24:29 +0530

    Add files via upload

---
+++ String8.java
@@ -0,0 +1,13 @@
+ class String8 {
+    public static void main(String[] args) {
+        String s = "OpenAI";
+        char ch;
+        System.out.println(s.charAt(2));
+        if(s.charAt(3)==s.charAt(4)){
+       System.out.println("same");
+        }else{
+            System.out.println("not sAME");
+        }
+
+    }
+}
